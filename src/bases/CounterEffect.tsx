41d06719eda6f190f5a0603deeb2bd2958745e78
gdc94 <germanderbescatoni@gmail.com>
2022-05-26 17:18:11 -0300

    Añadiendo animacion con GSAP

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,36 +1,39 @@
-import { useEffect, useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
 
 const MAXIMUN_COUNT = 10;
 
 export default function CounterEffect() {
   const [valor, setValor] = useState(1);
+  const counterElement = useRef<HTMLHeadingElement>(null);
 
   const increase = () => {
-    if(valor >= MAXIMUN_COUNT) return;
-    setValor(prev => prev + 1);
+    if (valor >= MAXIMUN_COUNT) return;
+    setValor((prev) => prev + 1);
   };
 
-
-
-
   useEffect(() => {
-    if(valor >= 10){
-      console.log('%cSe llegó al valor maáximo', 'color: green; background: black;');
-      
+    if (valor >= 10) {
+      console.log(
+        "%cSe llegó al valor maáximo",
+        "color: green; background: black;"
+      );
     }
-  }, [valor])
-  
 
+    const tl = gsap.timeline();
 
- 
+    tl.to(counterElement.current, {
+      y: -10,
+      duration: 0.2,
+      ease: "ease-out",
+    }).to(counterElement.current, { y: 0, duration: 1, ease: "bounce-out" });
+  }, [valor]);
 
   return (
     <>
       <h1>Esto es un contador</h1>
-      <p>{valor}</p>
+      <h2 ref={counterElement}>{valor}</h2>
       <button onClick={increase}> + </button>
-    
     </>
   );
 }
